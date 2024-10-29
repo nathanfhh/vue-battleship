7806e37adce949808d710fee65f75c2221d80858
傅暄桓 <t13762@ms3.sltung.com.tw>
2024-10-29 13:27:59 +0800

    add: vue dev tool

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
-  ],
+    plugins: [
+        vue(),
+        vuetify({autoImport: true}),
+        vueDevTools()
+    ],
 })
